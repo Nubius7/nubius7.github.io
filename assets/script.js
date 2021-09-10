757928c60f3d9577bae110bd3459f7e48201b2e5
Nubius <>
2021-09-10 16:25:57 +0200

    Trying to fix mobile appearance v6

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 window.onload = function() {
+    /* Scrolling */
     var heroContinue = document.getElementById('hero_continue');
     var enableScroll = document.getElementById('enable_scroll');
     var nav = document.getElementById('navigator');
 
     heroContinue.addEventListener('click', (event) => {
         event.preventDefault();
-        console.log('click');
         enableScroll.scrollIntoView({behavior: "smooth"});
     });
 
@@ -13,4 +13,11 @@ window.onload = function() {
         progress = window.scrollY / window.innerHeight;
         nav.style.opacity = progress * 0.85;
     });
+
+    /* Fixing Background resizing on mobile */
+     var background = document.getElementsByClassName('html');
+    window.addEventListener('resize', (event) => {
+        background.height = window.height + 60;
+    });
+    background.height = window.height + 60;
 }
